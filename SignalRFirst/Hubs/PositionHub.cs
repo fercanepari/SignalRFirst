de8efb84a6cda9ec128f3092dd720ca092888fb7
fercanepari <fercanepari@gmail.com>
2022-05-06 11:31:34 -0300

    Add project files.

---
+++ SignalRFirst/Hubs/PositionHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRFirst.Hubs
+{
+    public class PositionHub : Hub
+    {
+        public async Task SendPosition(int left, int top)
+        {
+            Console.WriteLine(left + " - " + top);
+            if(left < 0)
+            {
+                left = 0;
+            }
+            if (top < 0)
+            {
+                top = 0;
+            }
+            await Clients.Others.SendAsync("ReceivePosition", left, top);
+        }
+
+    }
+}
